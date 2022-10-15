3447d9b2a65592565115258cf1b8c0c78e7afaee
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-10-15 09:29:28 -0400

    Add wallet balance example for 0.3.0

---
+++ 0.3.0/wallet-balance.py
@@ -0,0 +1,34 @@
+import bdkpython as bdk
+
+
+descriptor = "wpkh(tprv8ZgxMBicQKsPcx5nBGsR63Pe8KnRUqmbJNENAfGftF3yuXoMMoVJJcYeUw5eVkm9WBPjWYt6HMWYJNesB5HaNVBaFc1M6dRjWSYnmewUMYy/84h/0h/0h/0/*)"
+db_config = bdk.DatabaseConfig.MEMORY()
+blockchain_config = bdk.BlockchainConfig.ELECTRUM(
+    bdk.ElectrumConfig(
+        "ssl://electrum.blockstream.info:60002",
+        None,
+        5,
+        None,
+        100
+    )
+)
+blockchain = bdk.Blockchain(blockchain_config)
+
+wallet = bdk.Wallet(
+             descriptor=descriptor,
+             change_descriptor=None,
+             network=bdk.Network.TESTNET,
+             database_config=db_config,
+         )
+
+# print new receive address
+address_info = wallet.get_address(bdk.AddressIndex.LAST_UNUSED)
+address = address_info.address
+index = address_info.index
+print(f"New BIP84 testnet address: {address} at index {index}")
+
+
+# print wallet balance
+wallet.sync(blockchain, None)
+balance = wallet.get_balance()
+print(f"Wallet balance is: {balance.total}")
